6bb6fb9421e5b610182898c44c0cdaeeeb615b59
Ebube-Ochemba <ebube17@gmail.com>
2024-08-16 00:43:45 +0100

    PEP 8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,7 +13,7 @@ def pascal_triangle(n):
         new_row = [1]
         for col in range(1, row):
             new_row.append(triangle[row - 1][col] +
-                              triangle[row - 1][col - 1])
+                           triangle[row - 1][col - 1])
         new_row.append(1)
         triangle.append(new_row)
     return triangle
