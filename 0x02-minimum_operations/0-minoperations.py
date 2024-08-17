731b45d6e1623fa00ebb24bced63943ce588bf98
Ebube-Ochemba <ebube17@gmail.com>
2024-08-17 03:39:57 +0100

    gotten prime factorization

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+
+
+def calcPrimeFactors(number):
+    """Calculates the prime factors of a given number.
+
+    return: A list of the prime factors of the input number.
+    """
+    div = 2
+    primes = []
+    rem_num = number
+
+    while (div <= rem_num / 2): # Any num > (num / 2) can't be a factor
+        if rem_num % div == 0:
+            primes.append(div)
+            rem_num //= div # reduce number
+        else:
+            div += 1
+
+    if rem_num > 1: # remaining factor greater > 1 must be prime
+        primes.append(int(rem_num))
+
+    return primes
+
+
+def minOperations(n):
+    """Calculates the fewest number of operations need
+    to result in exactly n H characters in the file"""
