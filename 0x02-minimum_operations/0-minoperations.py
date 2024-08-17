9a12f1e3cf06016f15ea7fa0b0f24a824ef7e7bf
Ebube-Ochemba <ebube17@gmail.com>
2024-08-17 04:06:23 +0100

    completed function

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,14 +11,14 @@ def calcPrimeFactors(number):
     primes = []
     rem_num = number
 
-    while (div <= rem_num / 2): # Any num > (num / 2) can't be a factor
+    while (div <= rem_num / 2):  # Any num > (num / 2) can't be a factor
         if rem_num % div == 0:
             primes.append(div)
-            rem_num //= div # reduce number
+            rem_num //= div  # reduce number
         else:
             div += 1
 
-    if rem_num > 1: # remaining factor greater > 1 must be prime
+    if rem_num > 1:  # remaining factor greater > 1 must be prime
         primes.append(int(rem_num))
 
     return primes
@@ -27,3 +27,11 @@ def calcPrimeFactors(number):
 def minOperations(n):
     """Calculates the fewest number of operations need
     to result in exactly n H characters in the file"""
+    if n <= 1:
+        return 0
+
+    # Get the prime factors of n
+    factors = calcPrimeFactors(n)
+
+    # The minimum operations are the sum of the prime factors
+    return sum(factors)
