8792afa7133b70f6d7afa6ed4e41cf8487fd0388
Ebube-Ochemba <ebube17@gmail.com>
2024-09-13 01:56:57 +0100

    illustration

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -17,3 +17,27 @@ def rotate_2d_matrix(matrix):
         for j in range(idx):
             # Python idiom for swapping values without a temporary variable.
             matrix[idx][j], matrix[j][idx] = matrix[j][idx], matrix[idx][j]
+
+
+"""
+illustration
+------------
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+--------------------
+STEPS:
+--------------------
+1. matrix.reverse():
+matrix = [[7, 8, 9],
+          [4, 5, 6],
+          [1, 2, 3]]
+--------------------
+2. "nested loop"
+idx=1, j=0:
+    matrix = [[7, 4, 9], [8, 5, 6], [1, 2, 3]]
+idx=2, j=0:
+    matrix = [[7, 4, 9], [8, 5, 6], [1, 2, 3]]
+idx=2, j=1:
+    matrix = [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+"""
